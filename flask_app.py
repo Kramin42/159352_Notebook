169e9f3a36071e2d55445483457202766124b9a6
Cameron Dykstra <dykstra.cameron@gmail.com>
2016-05-06 22:24:26 +0000

    added templates for login and register also added register code

--- flask_app.py
+++ flask_app.py
@@ -75,5 +75,21 @@ def index():
 #    db.session.commit()
 #    return redirect(url_for('index'))
 
+@app.route('/register' , methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'] , request.form['password'],request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     db.create_all()
