6a808521211fe69608026e91524ec846b6e96c59
Cameron Dykstra <dykstra.cameron@gmail.com>
2016-05-06 22:19:54 +0000

    Updated User model class with Flask-Login methods

--- flask_app.py
+++ flask_app.py
@@ -51,6 +51,21 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
 @app.route("/")
 def index():
     return render_template("main_page.html", entries=Entry.query.all())
